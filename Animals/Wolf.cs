02952db7b02d3b49992846bf72e8412dfd3f3549
Kamil Preweda <preweda.kamil@o2.pl>
2022-06-02 20:19:17 +0200

    Added Wolf subclass

---
+++ Animals/Wolf.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public class Wolf : Canine
+    {
+        public Wolf(bool belongsToPack)
+        {
+            BelongsToPack = belongsToPack;
+        }
+
+        public override void MakeNoise()
+        {
+            if (BelongsToPack) Console.WriteLine("Należę do stada.");
+            Console.WriteLine("Auuuuuuuu!");
+        }
+
+        public void HuntInPack()
+        {
+            if (BelongsToPack) Console.WriteLine("Wybieram się na polowanie z moim stadem!");
+            else Console.WriteLine("Nie należę do stada.");
+        }
+    }
+}
