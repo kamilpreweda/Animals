8fc054c23ebedb6099dfb0ac926695d23799ff55
Kamil Preweda <preweda.kamil@o2.pl>
2022-06-02 20:15:55 +0200

    Added Canine abstract class

---
+++ Animals/Canine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public abstract class Canine : Animal
+    {
+        public bool BelongsToPack { get; protected set; } = false;
+    }
+}
