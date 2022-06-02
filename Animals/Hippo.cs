3508d2905827d57ea5a4e39395207308cf0469a8
Kamil Preweda <preweda.kamil@o2.pl>
2022-06-02 20:14:40 +0200

    Added Hippo subclass

---
+++ Animals/Hippo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public class Hippo : Animal
+    {
+        public override void MakeNoise()
+        {
+            Console.WriteLine("Chrząkanie");
+        }
+
+        public void Swim()
+        {
+            Console.WriteLine("Plusk! Idę popływać");
+        }
+    }
+}
